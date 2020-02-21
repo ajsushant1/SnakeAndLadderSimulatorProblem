cfb65c3f271fc7534ab2936b7cca32c102876135
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-02-21 13:34:08 +0530

    ADDED: Function to check for options to move player position in the game

--- SnakeAndLadderSimulator.sh
+++ SnakeAndLadderSimulator.sh
@@ -2,16 +2,44 @@
 
 echo "/******************************************* WELCOME TO SNAKE AND LADDER SIMULATOR *******************************************/"
 
-# CONSTANT
+# CONSTANTS
 START_POSITION=0
+NO_PLAY=1
+LADDER=2
+SNAKE=3
 
-#VARIABLE
+#VARIABLES
 NumberOfPlayer=1
+playerPosition=$START_POSITION
+dieLimit=6
+optionsLimit=3
 rollDieValue=0
 
 # FUNCTION TO GET RANDOM VALUE
 function random(){
-	local randomValue=$((1+RANDOM%6))
+	local input=$1
+	local randomValue=$((1+RANDOM%$input))
 echo $randomValue
 }
-rollDieValue=$( random )
+
+# TO GET ROLLED DICE VALUE
+rollDieValue=$( random $dieLimit)
+
+# FUNCTION TO CHECK FOR OPTIONS OF GAME
+function checkOption(){
+	local choice=$( random $optionsLimit )
+	case $choice in
+		$NO_PLAY)
+			playerPosition=$playerPosition
+					;;
+		$LADDER)
+			playerPosition=$(($playerPosition+$rollDieValue))
+					;;
+		$SNAKE)
+			playerPosition=$(($playerPosition-$rollDieValue))
+					;;
+	esac
+}
+
+# FUNCTION CALL TO GET OPTIONS OF GAME
+checkOption
