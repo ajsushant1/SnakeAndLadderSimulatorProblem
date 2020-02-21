92db4dce7d2dda6ebf27375954642b79cf1f9653
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-02-21 12:30:52 +0530

    ADDED: Random function to get number between 1 to 6

--- SnakeAndLadderSimulator.sh
+++ SnakeAndLadderSimulator.sh
@@ -7,3 +7,11 @@ START_POSITION=0
 
 #VARIABLE
 NumberOfPlayer=1
+rollDieValue=0
+
+# FUNCTION TO GET RANDOM VALUE
+function random(){
+	local randomValue=$((1+RANDOM%6))
+echo $randomValue
+}
+rollDieValue=$( random )
